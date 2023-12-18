bd8b0e5ce6b29818ac7d8b3672dcf414e8df65aa
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2023-12-18 16:20:16 -0500

    add comments

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -3,51 +3,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const userInput = document.getElementById("user-input");
   const chatMessages = document.getElementById("chat-messages");
 
+  // event when click send button
   sendButton.addEventListener("click", function () {
     sendMessage();
   });
 
+  // event when click enter/return keyboard
   userInput.addEventListener("keydown", function (event){
     if (event.key === "Enter") {
       sendMessage();
     }
   });
 
+  // this function append messeges to the popup chat
   function appendMessage(sender, message) {
-    const messageDiv = document.createElement("div");
+    const messageDiv = document.createElement("div"); // create message div
+    // append message
     if (sender === "You") {
       messageDiv.innerHTML = `<div class="user-message"><strong>${sender}:</strong><span> ${message}</span></div>`;
     }
     else {
       messageDiv.innerHTML = `<div class="bot-message"><strong>${sender}:</strong></div><span>\n${message}</span>`;
     }
+    // add messages to chat-messages
     chatMessages.appendChild(messageDiv);
   };
   
+  // this function send message to content script
   async function sendMessage() {
     const userMessage = userInput.value;
+    // check input message, if not empty then append user message to chat-message
+    // then send message to content script to get response from backend
+    // then append the chatGPT to the chat-messages
     if (userMessage.trim() != "") {
-      appendMessage("You", userMessage);
-      // Add logic to send userMessage to backend and get the chatbot's response
-      // For simplicity, let's assume the response is obtained immediately
-      const message = "get-answer"
+      appendMessage("You", userMessage); // append user message
 
+      // send message to ask chatGPT
+      const message = "get-answer"
       const request = {
         message: message,
         data: userMessage
       }
-
       const response = await sendMessageToActiveTab(request);
 
+      // append response to chat-messages
       appendMessage("ChatGPT", response.answer);
 
       userInput.value = "";
     }
   }  
+
   async function sendMessageToActiveTab(message) {
+    // get the current tab
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     console.log(tab)
     try {
+      // send message to current tab content script
       const response = await chrome.tabs.sendMessage(tab.id, message);
       
       if (chrome.runtime.lastError) {
--- frontend/scripts/content.js
+++ frontend/scripts/content.js
@@ -1,24 +1,28 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  new Promise(async send => {
+  new Promise(async send => { // listen message async from popup
     if (message.message === 'get-answer') {
       console.log(message.data)
-      const tab = window.location.href;
+      const tab = window.location.href; // get the tab object
+      // create data object to send to back-end with videoID and user question
       const data = {user_input: message.data,
                     videoID: getYouTubeVideoId(tab)}
       const request = {
         method: 'POST',
         body: JSON.stringify(data)
       }
+      // send to backend and wait for response from backend
       const answer = await talk_to_backend('http://localhost:5000/response',request);
+      // response object to send response back to popup
       const response = {
         message: 'responsed',
         answer: answer.answer
       }
       console.log(response)
+      // send the response
       send(response)
     }
   }).then(sendResponse)
-  
+  // always return true when usinf async
   return true;
 });
 
@@ -29,7 +33,9 @@ function getYouTubeVideoId(tab) {
 
 async function talk_to_backend(url,request) {
   try {
+    // fetch to the url network from backend
     const response = await fetch(url,request);
+    // wait for response
     const data = await response.json()
     return data;
   } catch (error) {

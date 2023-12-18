114b82283de92368e6306d6b220a854243baf434
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2023-12-18 02:04:34 -0500

    update

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-OpenAI_API_KEY = "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"
+OpenAI_API_KEY = "your-api"
 OpenAI_API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions"
 GPT_MODEL = "gpt-3.5-turbo"
 PORT = 5000

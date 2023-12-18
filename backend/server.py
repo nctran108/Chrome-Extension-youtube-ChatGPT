256b14ce41814f62a4961cced8d2630d5f7cea60
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2023-12-18 16:21:14 -0500

    add comments

--- backend/server.py
+++ backend/server.py
@@ -10,43 +10,36 @@ CORS(app)
 client = OpenAI(api_key=config.OpenAI_API_KEY)
 os.environ["OPEN_API_KEY"] = config.OpenAI_API_KEY
 
-
-           
-@app.route('/transcript', methods=['POST'])
-def transcript():
-    data = json.loads(request.data)
-    print("Message received in Python backend-string:", data['videoID'])
-    # Send a response back to the extension if needed
-    transcript = get_transcript(data['videoID'])
-    answers = get_answers(transcript)
-    print(answers)
-    response = {
-        "transcript": "OpenAI Answered The Transcript!"
-    }
-    return jsonify(response), 200
-
-def get_transcript(video_id):
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    transcript_msg = ' '.join([f"{data['start']}: {data['text']}," for data in transcript])
-    print(transcript_msg)
-    return transcript_msg
-
 @app.route('/response', methods=['POST'])
 def response_msg():
+    # load data from frontend
     data = json.loads(request.data)
+    # get video transcript
     transcript = get_transcript(data['videoID'])
+    # add user question to the transcript
     user_input = data['user_input']
     user_input = transcript + "\n" + user_input
+    # ask openai
     response = {
         "answer": get_answers(user_input)
     }
+    # return the response to frontend
     return jsonify(response), 200
+
+def get_transcript(video_id):
+    # use youtube api to get the video transcript
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    # convert it to string that include time and transcript
+    transcript_msg = ' '.join([f"{data['start']}: {data['text']}," for data in transcript])
+    print(transcript_msg)
+    return transcript_msg
 
 def get_answers(prompt):
     messages =[{
         "role": "user",
          "content": prompt
     }]
+    # get openai response
     response = client.chat.completions.create(model=config.GPT_MODEL, messages=messages)
     return response.choices[0].message.content.strip()
 
